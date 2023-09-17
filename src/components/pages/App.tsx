8128e83846e192925bdcd77a40ac9741768d0a2c
BShabowski <benshabowski@gmail.com>
2023-09-17 17:14:28 -0500

    Starting it up

---
+++ src/components/pages/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../style/App.css';
+
+function App() {
+  return (
+      <div className="App">
+          <p>A new beginning</p>
+      </div>
+  );
+}
+
+export default App;
