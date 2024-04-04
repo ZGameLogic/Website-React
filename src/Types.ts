1a4e4660055789f6ef6011ba8e3791e5aa491671
Ben Shabowski <benshabowski@gmail.com>
2024-04-04 15:05:06 -0500

    Adding in some charts 2

---
+++ src/Types.ts
@@ -0,0 +1,14 @@
+export type SOTDataPoint = {
+    id: number;
+    ben: boolean;
+    patrick: boolean;
+    jj: boolean;
+    greg: boolean;
+    success: boolean;
+    proposed: Date;
+};
+
+export type SuccessRate = {
+    name: string,
+    rate: number
+}
